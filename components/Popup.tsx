6ef3ea30968ce777ecbdf4edcd43a841cc9df294
yk123 <yk@idealtech.com.my>
2024-10-28 16:55:49 +0800

    changed max-w to fit

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -32,7 +32,7 @@ const Popup: React.FC<PopupMessageProps> = ({ message, type, duration = 3000 }) 
 
     return (
         <div
-            className={`fixed bottom-20 left-1/2 transform -translate-x-1/2 z-50 max-w-xs w-full p-4 rounded-lg shadow-lg text-white transition-opacity duration-500 ease-in-out
+            className={`fixed bottom-20 left-1/2 transform -translate-x-1/2 z-50 max-w-fit w-full p-4 rounded-lg shadow-lg text-white transition-opacity duration-500 ease-in-out
         ${visible ? 'opacity-100' : 'opacity-0'} 
         ${type === 'success' ? 'bg-green-800' : 'bg-red-700'}`}
             style={{
@@ -43,9 +43,9 @@ const Popup: React.FC<PopupMessageProps> = ({ message, type, duration = 3000 }) 
             <div className="flex justify-between items-center">
                 <div className="flex items-center space-x-2">
                     {type === 'success' ? (
-                        <MdCheckCircle className="text-white text-xl" />
+                        <MdCheckCircle size={20} className="text-white" />
                     ) : (
-                        <MdError className="text-white text-xl" />
+                        <MdError size={20} className="text-white" />
                     )}
                     <span className="font-inter font-sm">{message}</span>
                 </div>

c71a4d79db75a38aef86f1643b68d7274835b2e0
yk123 <yk@idealtech.com.my>
2025-01-04 17:24:03 +0800

    user level ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import ResultsPage from "@/components/ResultsPage";
-import EntryAnalyzer from "@/components/EntryAnalyzer";
+import UserResultsPage from "@/components/UserResultsPage";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full mx-auto mb-60">
-      <EntryAnalyzer />
-      <ResultsPage />
+    <UserResultsPage />
     </div>
 
 

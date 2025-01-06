f10d4aa59347c76c50e51025eb6829d322809f3f
yk123 <yk@idealtech.com.my>
2025-01-06 13:05:10 +0800

    userResultsPage no longer needed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import UserResultsPage from "@/components/UserResultsPage";
+import ResultsPage from "@/components/ResultsPage";
 
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full mx-auto mb-60">
-    <UserResultsPage />
+    <ResultsPage />
     </div>
 
 
